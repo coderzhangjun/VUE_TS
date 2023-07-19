d27016286fcc6c78c1f767cb2a2b0d70511094c4
coderzhangjun <573100283@qq.com>
2023-07-19 23:46:34 +0800

    bg

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="">我是login</div>
+  <div class="login">我是login</div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="less" scoped></style>
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  background: url('../../assets/img/login-bg.svg');
+  width: 100%;
+  height: 100%;
+}
+</style>
