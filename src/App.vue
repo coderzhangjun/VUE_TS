5d70605e94f6ad60a11997b8bc2699443e1d5db9
zhangJun <573100283@qq.com>
2023-07-25 23:08:48 +0800

    Your commit message

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,14 @@ import { Check, Delete, Edit, Message, Search, Star } from '@element-plus/icons-
 </script>
 
 <template>
-<<<<<<< HEAD
   <div class="app">
     <router-view></router-view>
   </div>
-=======
-  <div>我是APP啊0619</div>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">111</el-button>
-    <el-button type="success">22</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
-  <router-view></router-view>
->>>>>>> aeeea9c46d0660f098f1d120b8b65981915e10aa
 </template>
 
 <style scoped>
-  .app{
-    width: 100%;
-    height: 100%;
-  }
+.app {
+  width: 100%;
+  height: 100%;
+}
 </style>
