b0413b42c886f48aac1227a8cc38a666c0d0a168
zhangJun <573100283@qq.com>
2023-07-25 23:34:13 +0800

    1

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="login">我是login</div>
+  <div class="login">
+    <div class="loginCheck">
+      <h1>弘源后台管理系统</h1>
+      <div>
+        登录
+      </div>
+      <div>
+        记住密码 忘记密码
+      </div>
+      <button>立即登录</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
@@ -8,8 +19,11 @@
 .login {
   display: flex;
   justify-content: center;
+  align-items: center;
   background: url('../../assets/img/login-bg.svg');
   width: 100%;
   height: 100%;
 }
+
+/* .loginCheck {} */
 </style>
